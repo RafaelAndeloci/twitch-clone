9861b97c40a8b45753f873a2d1723f030966504b
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2024-07-30 16:13:49 -0300

    setup: adding auth with clerk and shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider afterSignOutUrl={"/"}>
+      <html lang="en">
+        <body className={cn(inter.className, "antialiased")}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
